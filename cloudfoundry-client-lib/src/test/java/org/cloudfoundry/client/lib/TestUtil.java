d553f8f387fe98019b57d3005f3df7310c6e4641
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2019-07-25 18:13:05 +0300

    Find the correct domain when there is an empty route

---
+++ cloudfoundry-client-lib/src/test/java/org/cloudfoundry/client/lib/TestUtil.java
@@ -0,0 +1,25 @@
+package org.cloudfoundry.client.lib;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public final class TestUtil {
+
+    private TestUtil() {
+
+    }
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String readFileContent(String fileName, Class<?> clazz) throws IOException {
+        InputStream resource = clazz.getResourceAsStream(fileName);
+        return IOUtils.toString(resource);
+    }
+
+    public static <T> T fromJson(String content, Class<T> clazz) throws IOException {
+        return objectMapper.readValue(content, clazz);
+    }
+}
